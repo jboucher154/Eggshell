260e00dd0d32d52f62d3b9d0257771b8fc65830b
ShereeAnne <92857375+ShereeAnne@users.noreply.github.com>
2023-04-07 16:04:12 +0300

    shell loop and reading line

---
+++ Makefile
@@ -0,0 +1,52 @@
+NAME := minishell
+
+LIB = libft/libft.a
+
+CC := CC
+
+CFLAGS := -Wall -Wextra -Werror -I includes -g #-fsanitize=address
+
+INCS := includes/minishell.h
+#need to add handling for compling from srcs subdirs
+SRC_DIR := srcs
+
+OBJ_DIR := objs
+
+SRCS := main.c 
+
+SRCS := $(SRCS:%=$(SRC_DIR)/%)
+
+OBJS := $(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+
+READLINE := -lreadline -L ~/.brew/opt/readline/lib -I ~/.brew/opt/readline/include #from Lucas
+
+all: $(OBJ_DIR) $(NAME)
+
+$(NAME): $(OBJS) $(LIB)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(LIB) $(READLINE)
+	@echo "\033[0;92m* $(NAME) was created *\033[0m"
+
+$(OBJ_DIR):
+	@mkdir $(OBJ_DIR)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIB): 
+	$(MAKE) -C libft
+
+clean:
+	@rm -f $(OBJS)
+	@rm -rf $(OBJ_DIR)
+	$(MAKE) clean -C libft
+	@echo "\033[0;91m* $(NAME) .o files and obj directory were deleted *\033[0m"
+
+fclean: clean
+	@rm -f $(NAME)
+	$(MAKE) fclean -C libft
+	@echo "\033[0;91m* $(NAME) was deleted *\033[0m"
+
+re: fclean all
+	
+.PHONY: all clean fclean re
+.SILENT:
---
+++ includes/minishell.h
@@ -0,0 +1,69 @@
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+// colours
+#define GREEN "\e[1;92m"
+#define KNRM  "\x1B[0m"
+#define PROMPT "\e[1;92m🐣[EggShell] % \x1B[0m"
+
+
+#include "libft.h"
+
+// for printf, perror
+#include <stdio.h> 
+
+//need includes for readline
+#include <readline/readline.h>
+#include <readline/history.h>
+
+//for malloc, free, exit, ttyslot(System V-like systems), getenv
+#include <stdlib.h>
+
+//for write, access, read, close, fork, getcwd, chdir, stat, lstat, fstat, 
+// unlink, execve, dup, dup2, pipe, isatty, ttyname, ttyslot(BSD-like systems and Linux)
+// tcsetattr, tcgetattr
+#include <unistd.h>
+
+//for open,
+#include <fcntl.h>
+
+//for wait, waitpid, wait3, wait4
+#include <sys/wait.h>
+
+//for signal, sigaction, sigemptyset, sigaddset, kill
+#include <signal.h>
+
+//for opendir, stat, lstat, fstat, closedir
+#include <sys/types.h>
+
+//for readdir, opendir, closefir
+#include <dirent.h>
+
+//for stat, lstat, fstat
+#include <sys/stat.h>
+
+//for sterror
+#include <string.h>
+
+//for ioctl
+#include <sys/ioctl.h>
+
+//for tcsetattr, tcgetattr
+#include <termios.h>
+
+//for tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+#include <curses.h>
+
+//for tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+#include <term.h>
+
+//for error codes
+#include <errno.h>
+
+
+
+//functions for PARSER
+
+
+
+#endif
