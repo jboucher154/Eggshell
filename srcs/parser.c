f5b60ec5c34ab7eded8ed6616242d76de2331a2d
Jenny Boucher <jebouche@c3r4p5.hive.fi>
2023-04-14 17:30:44 +0300

    add advacing past token for pipe and line so evals start on next character

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 13:46:02 by jebouche          #+#    #+#             */
-/*   Updated: 2023/04/14 17:09:44 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/04/14 17:28:42 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ t_cmd	*handle_pipe(char **parsed_string, char *end)
 	if (peek_next_token(*parsed_string, "|"))
 	{
 		move_to_token(parsed_string, end);
+		(*parsed_string)++; //move past the token so new command can process
 		cmd = new_pipe(cmd, handle_pipe(parsed_string, end));
 	}
 	return (cmd);
@@ -37,6 +38,7 @@ t_cmd	*parse_line(char **parsed_string, char *end)
 	if (peek_next_token(*parsed_string, ";")) //should I peek the next token?
 	{
 		move_to_token(parsed_string, end);
+		(*parsed_string)++; //move past the token so new command can process
 		cmd = new_line(cmd, parse_line(parsed_string, end));
 	}
 	return (cmd);
@@ -58,14 +60,14 @@ t_cmd	*parser(char *input_string)
 }
 
 /*
-** make initial tree
+** make initial tree √
 ** traverse tree and expand values if necessary, enter null terminators
-** return tree head
+** return tree head √
 */
 
 int	main(void)
 {
-	t_cmd *cmd_tree = parser("< infile_me test 'this'");
+	t_cmd *cmd_tree = parser("< infile_me test 'this' | echo me");
 	printf("head node type: %i\n", cmd_tree->type);
 	if  (cmd_tree->type == 0)
 	{
