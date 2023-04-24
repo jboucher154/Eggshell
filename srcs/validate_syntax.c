0db47969e429924bb2e7ad5f058e4617c0f5dab2
ShereeAnne <92857375+ShereeAnne@users.noreply.github.com>
2023-04-24 11:20:19 +0300

    add initial handling for validating syntax in str before tree

--- srcs/validate_syntax.c
+++ srcs/validate_syntax.c
@@ -12,17 +12,34 @@
 
 #include "minishell.h"
 #include "ft_ast.h"
+#define TRUE 1
+#define FALSE 0
 
-// int	validate_alpha(char *token_start)
+
+int	validate_pipe(char *token, char *str)
+{
+		if(token == str || token[-1] == '<' || token[-1] == ';')
+		{
+			printf("%sEggShell🥚: syntax error near unexpected token `|'%s\n", RED, KNRM);
+			return (FALSE);
+		}
+		if(token[1] == ';')
+		{
+			printf("%sEggShell🥚: syntax error near unexpected token `|'%s\n", RED, KNRM);
+			return (FALSE);
+		}
+		if(token[-1] == '<' && token[-2] == '<')
+		{
+			printf("%sEggShell🥚: syntax error near unexpected token `|'%s\n", RED, KNRM);
+			return (FALSE);
+		}
+ 	return (TRUE);
+}
+
+// void  validate_quotes(char *line, char symbol)
 // {
-// 	return (TRUE);
+    //check for matching parenthases, quotes.
 // }
-
-// int	validate_pipe(char *token_start)
-// {
-// 	return (TRUE);
-// }
-
 
 // int	validate_redirect_in(char *token_start)
 // {
@@ -39,26 +56,48 @@
 // 	return (TRUE);
 // }
 
-// //might not be needed?
-// int	validate_semicolon(char *token_start)
-// {
-// 	return (TRUE);
-// }
+int	validate_syntax(char *str)
+{
+		char token_id;
+		char *token;
+		int valid;
 
-// //maybe this driver isn't needed? can call a validation everytime we ID a token in the parser?
-// int	validate_syntax(char *token_start, char token_id)
-// {
-	// 	if (token_id == ALPHA)
-	// 		valid = validate_alpha(token_start); //validate alpha syntax
-	// 	else if (token_id == PIPE)
-	// 		valid = validate_pipe(token_start);	//validate pipe syntax
-	// 	else if (token_id == REDIRECT_IN)
-	// 		valid = validate_redirect_in(token_start);	//validate redirect in syntax
-	// 	else if (token_id == REDIRECT_OUT)
-	// 		valid = validate_redirect_out(token_start);	//validate redirect out syntax
-	// 	else if (token_id == REDIRECT_OUT_APPEND)
-	// 		valid = validate_redirect_out_append(token_start);	//validate redirect out append syntax
-	// 	else if (token_id == SEMICOLON)
-	// 		valid = validate_semicolon(token_start);	//validate semicolon syntax
-	// 	return (valid);
-// }
+		token = str;
+		valid = TRUE;
+		while (token && valid)
+		{
+			printf("\ninside loop\n");
+			token = ft_strchr(TOKENS, *token);
+			if (!token)
+				break ;
+			token_id = identify_token(token);
+			if (token_id == PIPE)
+			{
+				printf("\n entering validate pipe\n");
+				valid = validate_pipe(token, str);	//validate pipe syntax
+			}
+			// else if (token_id == REDIRECT_IN)
+			// 	valid = validate_redirect_in(token_start);	//validate redirect in syntax
+			// else if (token_id == REDIRECT_OUT)
+			// 	valid = validate_redirect_out(token_start);	//validate redirect out syntax
+			// else if (token_id == REDIRECT_OUT_APPEND)
+			// 	valid = validate_redirect_out_append(token_start);	//validate redirect out append syntax
+			// else if (token_id == REDIRECT_HERE)
+			// 	valid = validate_redirect_out_append(token_start);	//validate redirect out append syntax
+			// if (token == REDIRECT_OUT_APPEND || token == REDIRECT_HERE)
+			// 	token = token + 2;
+			// else 
+				token++;
+		}
+		// if (token_id == ALPHA)
+		// 	valid = validate_commands(token_start); //validate commands syntax
+		return (valid);
+}
+
+int main(void)
+{
+	char *str = "how does this print";
+
+	printf("%i\n", validate_syntax(str));
+	return (0);
+}
