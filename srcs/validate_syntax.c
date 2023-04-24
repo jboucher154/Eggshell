5577e5306815065bf954e05e06337b1b15ef53c3
Jenny Boucher <jebouche@c2r4p5.hive.fi>
2023-04-24 16:40:42 +0300

    created a file syntax check

--- srcs/validate_syntax.c
+++ srcs/validate_syntax.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 14:36:05 by jebouche          #+#    #+#             */
-/*   Updated: 2023/04/20 14:53:31 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/04/24 16:31:41 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,89 +15,108 @@
 #define TRUE 1
 #define FALSE 0
 
-
 int	validate_pipe(char *token, char *str)
 {
-		if(token == str || token[-1] == '<' || token[-1] == ';')
+		if(token == str || token[-1] == '<' || token[-1] == ';' || \
+			token[1] == ';')
 		{
-			printf("%sEggShell🥚: syntax error near unexpected token `|'%s\n", RED, KNRM);
+			ft_putstr_fd("\033[31mEggShell🥚: syntax error near unexpected token `|'\x1B[0m\n", 2);
 			return (FALSE);
 		}
-		if(token[1] == ';')
+		if (token[1] == '|')
 		{
-			printf("%sEggShell🥚: syntax error near unexpected token `|'%s\n", RED, KNRM);
-			return (FALSE);
+			ft_putstr_fd("\033[31mEggShell🥚: Error, we did not do the bonus!\x1B[0m\n", 2);
+			return (FALSE);	
 		}
 		if(token[-1] == '<' && token[-2] == '<')
 		{
-			printf("%sEggShell🥚: syntax error near unexpected token `|'%s\n", RED, KNRM);
+			ft_putstr_fd("\033[31mEggShell🥚: syntax error near unexpected token `|'\x1B[0m\n", 2);
 			return (FALSE);
 		}
  	return (TRUE);
 }
 
-// void  validate_quotes(char *line, char symbol)
-// {
-    //check for matching parenthases, quotes.
-// }
+int	validate_quotes(char **token)
+{
+	char	quote_to_match;
 
-// int	validate_redirect_in(char *token_start)
-// {
-// 	return (TRUE);
-// }
+	quote_to_match = **token;
+	(*token)++;
+	while (**token && **token != quote_to_match)
+		(*token)++;
+	if(!**token)
+	{
+		ft_putstr_fd("\033[31mEggShell🥚: syntax error unclosed quotes\x1B[0m\n", 2);
+		return (FALSE);
+	}
+	else
+		return (TRUE);
+}
 
-// int	validate_redirect_out(char *token_start)
-// {
-// 	return (TRUE);
-// }
+int	validate_redirect_in(char *token)
+{
+	printf("redirect in entered\n");
+	if (ft_strchr(WHITESPACE, token[1]) == NULL && ft_isalpha(token[1]) == 0 && token[1] != '$')
+	{
+		ft_putstr_fd("\033[31mEggShell🥚: syntax error near unexpected token `>'\x1B[0m\n", 2);
+		return (FALSE);
+	}
+	return (TRUE);
+}
 
-// int	validate_redirect_out_append(char *token_start)
-// {
-// 	return (TRUE);
-// }
+int	validate_redirect_out(char *token)
+{
+		printf("redirect out entered\n");
+	if (ft_strchr(WHITESPACE, token[1]) == NULL && ft_isalpha(token[1]) != 1 && \
+		token[1] != '$')
+	{
+		ft_putstr_fd("\033[31mEggShell🥚: syntax error near unexpected token `>'\x1B[0m\n", 2);
+		return (FALSE);
+	}
+	return (TRUE);
+}
+
+int	validate_redirect_out_append(char *token)
+{
+	if (ft_strchr(WHITESPACE, token[0]) == NULL && ft_isalpha(token[0]) == 0 && \
+		token[0] != '$')
+	{
+		ft_putstr_fd("\033[31mEggShell🥚: syntax error near unexpected token `>'\x1B[0m\n", 2);
+		return (FALSE);
+	}
+	return (TRUE);
+}
 
 int	validate_syntax(char *str)
 {
 		char token_id;
 		char *token;
 		int valid;
-
+		
 		token = str;
-		valid = TRUE;
-		while (token && valid)
+		valid = 4;
+		while (*token && valid)
 		{
-			printf("\ninside loop\n");
-			token = ft_strchr(TOKENS, *token);
-			if (!token)
+			while (*token && ft_strchr(TOKENS, *token) == NULL && ft_strchr(QUOTES, *token) == NULL)
+				token++;
+			if (!*token)
 				break ;
 			token_id = identify_token(token);
 			if (token_id == PIPE)
+				valid = validate_pipe(token, str);
+			else if (*token == '"' || *token == '\'')
+				valid = validate_quotes(&token);
+			else if (token_id == REDIRECT_IN)
+				valid = validate_redirect_in(token); //validate redirect in syntax
+			else if (token_id == REDIRECT_OUT)
+				valid = validate_redirect_out(token);	//validate redirect out syntax
+			if (token_id == REDIRECT_OUT_APPEND || token_id == REDIRECT_HERE)
 			{
-				printf("\n entering validate pipe\n");
-				valid = validate_pipe(token, str);	//validate pipe syntax
+				token = token + 2;
+				valid = validate_redirect_out_append(token);
 			}
-			// else if (token_id == REDIRECT_IN)
-			// 	valid = validate_redirect_in(token_start);	//validate redirect in syntax
-			// else if (token_id == REDIRECT_OUT)
-			// 	valid = validate_redirect_out(token_start);	//validate redirect out syntax
-			// else if (token_id == REDIRECT_OUT_APPEND)
-			// 	valid = validate_redirect_out_append(token_start);	//validate redirect out append syntax
-			// else if (token_id == REDIRECT_HERE)
-			// 	valid = validate_redirect_out_append(token_start);	//validate redirect out append syntax
-			// if (token == REDIRECT_OUT_APPEND || token == REDIRECT_HERE)
-			// 	token = token + 2;
-			// else 
+			if (valid)
 				token++;
 		}
-		// if (token_id == ALPHA)
-		// 	valid = validate_commands(token_start); //validate commands syntax
 		return (valid);
-}
-
-int main(void)
-{
-	char *str = "how does this print";
-
-	printf("%i\n", validate_syntax(str));
-	return (0);
 }
