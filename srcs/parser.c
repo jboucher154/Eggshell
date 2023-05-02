af7519c528ac78d8ddf82af7760426b0dc97bd95
Jenny Boucher <jebouche@c2r3p5.hive.fi>
2023-05-02 10:53:01 +0300

    move tracking of cmd and pipe count to eggcarton variables

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,26 +6,25 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 13:46:02 by jebouche          #+#    #+#             */
-/*   Updated: 2023/04/27 13:56:03 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/05/02 10:36:21 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //need to add in NULL checks for allocated nodes
-
-t_cmd	*handle_pipe(char **parsed_string, char *end, int *cmd_count, int *pipe_count, t_eggcarton *prog_info)
+t_cmd	*handle_pipe(char **parsed_string, char *end, t_eggcarton *prog_info)
 {
 	t_cmd	*cmd;
 
-	cmd = handle_exec(parsed_string, end, cmd_count, prog_info);
+	cmd = handle_exec(parsed_string, end, prog_info);
 	if (peek_next_token(*parsed_string, "|"))
 	{
 		move_to_token(parsed_string, end);
 		(*parsed_string)++;
 		move_pointer_past_ws(parsed_string);
-		(*pipe_count)++;
-		cmd = new_pipe(cmd, handle_pipe(parsed_string, end, cmd_count, pipe_count, prog_info));
+		(prog_info->pipe_count)++;
+		cmd = new_pipe(cmd, handle_pipe(parsed_string, end, prog_info));
 	}
 	return (cmd);
 }
@@ -41,6 +40,6 @@ t_cmd	*parser(char *input_string, t_eggcarton *prog_info)
 	end_parse = input_string + (ft_strlen(input_string));
 	move_pointer_past_ws(&parsed_string);
 	if (parsed_string && *parsed_string)
-		cmd = handle_pipe(&parsed_string, end_parse, &(prog_info->cmd_count), &(prog_info->pipe_count), prog_info);
+		cmd = handle_pipe(&parsed_string, end_parse, prog_info);
 	return (cmd);
 }
