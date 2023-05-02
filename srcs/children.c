78dae17f4e8d04f75e4c8b1899caadd713dd6021
Jenny Boucher <jebouche@c2r3p5.hive.fi>
2023-05-02 08:51:51 +0300

    norm formatting

--- srcs/children.c
+++ srcs/children.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 15:44:45 by jebouche          #+#    #+#             */
-/*   Updated: 2023/04/27 11:00:23 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/05/02 08:50:24 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_child	*new_child(void)
 		return (NULL);
 	child->path = NULL;
 	child->args = NULL;
-	child->command_present = TRUE;// added to track if it is for redirects only, default is true
+	child->command_present = TRUE;
 	child->redir_in = UNSET;
 	child->redir_out = UNSET;
 	child->pipe_in = UNSET;
@@ -36,7 +36,8 @@ void	free_children(t_child **children)
 	index = 0;
 	while (children[index])
 	{
-		close_redirections(children[index]->redir_in, children[index]->redir_out);
+		close_redirections(children[index]->redir_in, \
+		children[index]->redir_out);
 		free(children[index]);
 		index++;
 	}
@@ -48,15 +49,15 @@ int	create_child_array(t_eggcarton *prog_info)
 {
 	t_child	**children;
 	int		index;
-	int		child_count;//
+	int		child_count;
 
-	child_count = 1 + prog_info->pipe_count;//
-	children = (t_child **) malloc(sizeof(t_child *) * (child_count + 1));//change allocation to make children based on pipe count. This is so we can run redirection only child tasks
+	child_count = 1 + prog_info->pipe_count;
+	children = (t_child **) malloc(sizeof(t_child *) * (child_count + 1));
 	if (!children)
 		return (print_error("malloc failed"));
 	index = 0;
 	children[prog_info->cmd_count] = NULL;
-	while(index < prog_info->cmd_count)
+	while (index < prog_info->cmd_count)
 	{
 		children[index] = new_child();
 		if (!children[index])
