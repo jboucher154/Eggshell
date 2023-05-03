c0f92862bda2b227cd37db4779fa4ad96fdf7199
Jenny Boucher <jebouche@c2r3p5.hive.fi>
2023-05-03 15:42:55 +0300

    remove export functions

--- srcs/builtins/env_builtins.c
+++ srcs/builtins/env_builtins.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:14:27 by smorphet          #+#    #+#             */
-/*   Updated: 2023/05/03 10:43:45 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/05/03 15:39:22 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,43 +29,3 @@ void	unset_command(char **args, t_hash_table *ht_env)
 	}
 }
 
-void	set_new_env_variable(char *arg, t_hash_table *environment)
-{
-	int		arg_index;
-	char	*key;
-	char	*value;
-
-	arg_index = 0;
-	while (arg[arg_index] != '\0' && arg[arg_index] != '=')
-		arg_index++;
-	key = ft_substr(arg, 0, arg_index);
-	if (arg[arg_index] == '\0')
-		value = NULL;
-	else
-	{
-		arg_index++;
-		value = ft_strdup(arg + arg_index);
-	}
-	ht_add(environment, key, value);
-	free(key);
-}
-
-void	export_command(char **args, t_hash_table *environment)
-{
-	int	index;
-	int	added;
-
-	added = 0;
-	index = 1;
-	while (args[index])
-	{
-		if (args[index][0])
-		{
-			set_new_env_variable(args[index], environment);
-			added++;
-		}
-		index++;
-	}
-	if (added == 0)
-		ht_print_export(environment);
-}
