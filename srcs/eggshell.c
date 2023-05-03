ee58c1fceeb1c8b3d8ecca6dc9d3c029f89ca73c
Jenny Boucher <jebouche@c2r3p5.hive.fi>
2023-05-03 15:43:58 +0300

    free line as soon as done parsing

--- srcs/eggshell.c
+++ srcs/eggshell.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 15:25:23 by jebouche          #+#    #+#             */
-/*   Updated: 2023/05/02 09:03:43 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/05/03 15:24:07 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,26 @@ static char	*ft_gets(void)
 }
 
 //add clean program exit when exited
-static int	parse_and_execute(char *line, t_eggcarton *prog_info)
+static int	parse_and_execute(char **line, t_eggcarton *prog_info)
 {
 	t_cmd	*cmd_tree;
 	int		status;
 
 	status = 0;
 	cmd_tree = NULL;
-	if (!ft_strncmp("EXIT", line, 4))
+	if (!ft_strncmp("EXIT", *line, 4))
 	{
 		status = 1;
 		//clean program function needed
 	}
 	else
 	{
-		cmd_tree = parser(line, prog_info);
+		cmd_tree = parser(*line, prog_info);
 		print_tree(cmd_tree, 0); //
+		free(*line);
+		*line  = NULL;
 		if (cmd_tree)
-			executer(cmd_tree, prog_info);
+			executer(&cmd_tree, prog_info);
 		reset_program(prog_info, &cmd_tree);
 	}
 	return (status);
@@ -70,9 +72,9 @@ int	eggshell(t_eggcarton *prog_info)
 			break ;
 		//	exit(0); //EXIT CLEAN UP ETC
 		}
-		if (line && *line && validate_syntax(line))
+		if (line && *line && validate_syntax(line, prog_info))
 		{
-			status = parse_and_execute(line, prog_info);
+			status = parse_and_execute(&line, prog_info);
 		}
 		if (line)
 			free(line);
