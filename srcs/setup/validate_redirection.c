bdcf236d33506fdcad1020857f238825d262eab8
Jenny Boucher <jebouche@c2r4p5.hive.fi>
2023-05-17 13:59:44 +0300

    change validation for redirect in and out to reject > >

--- srcs/setup/validate_redirection.c
+++ srcs/setup/validate_redirection.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 16:14:19 by jebouche          #+#    #+#             */
-/*   Updated: 2023/05/04 09:38:35 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/05/17 13:56:48 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 int	validate_redirect(char **token, char token_id)
 {
+	(void) token_id;//
 	(*token)++;
 	move_pointer_past_ws(token);
-	printf("token at top of validate redirect: %s\n", *token);//
+	printf("token at top of validate redirect: |%s|\n", *token);//
 	if (!(**token))
 		return (print_error("syntax error near unexpected token `newline'"));
-	if ((token_id == REDIRECT_IN && **token == '>') || \
-		(token_id == REDIRECT_OUT && **token == '<'))
+	// if ((token_id == REDIRECT_IN && **token == '>') || \
+	// 	(token_id == REDIRECT_OUT && **token == '<'))
+	// 	return (print_error("Syntax error, unexpected token"));
+	if ( **token == '>' || **token == '<')
 		return (print_error("Syntax error, unexpected token"));
 	return (TRUE);
 }
